36ccf59e6304368b30b949e9ad362ffa0846971b
lqqnjust <luqq1210@gmail.com>
2018-09-25 23:21:31 +0800

    更新

--- sanguo/scenes/battle_scene.py
+++ sanguo/scenes/battle_scene.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 from cocos.scene import Scene
+from pytmx.util_pyglet import pyglet_image_loader,load_pyglet
 
 from .layer import BattleLayer
 from .layer import GeneralInfoLayer
@@ -8,6 +9,10 @@ from .layer import GeneralInfoLayer
 class BattleScene(Scene):
     def __init__(self, *children):
         super().__init__(*children)
+
+        tmx_data = load_pyglet("sanguo/resource/smallbattle/water.tmx")
+        layer = tmx_data['layer1']
+        self.add(layer)
 
         layer = BattleLayer()
         self.add(layer)
@@ -21,3 +26,5 @@ class BattleScene(Scene):
 
         infolayer = GeneralInfoLayer(generalmodel)
         self.add(infolayer)
+
+
